2b90dd50be51bba44023294bf3e5bcd2bad33f26
R33dah <99521096+R33dah@users.noreply.github.com>
2022-03-17 22:51:20 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,11 @@
 
 /**
   * mul - Multiplies two integers
-  * @a: The first number to multiply
+ * @a: The first number to multiply
   * @b: The second number to multiply with first number
   *
   * Return: The result of multiplication between a param and b param
-  */
+  
 int mul(int a, int b)
 {
 	return (a * b);
