d323b065379f23f56474036d346ed141eea0c5fa
r33edah <little.musahh@gmail.com>
2022-03-22 00:10:34 +0100

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main_h" 
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[.[C[C[
+
+/**
+
+ *reset_to_98- function that takes a pointer to an int as paramete[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D [C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cr
+
+ * and updates the value it points to to 98.
+ *
+ *@n: mumber.
+ *
+ */
+
+void reset_to_98(int *n);
+{
+
+[A[A[([*n = 98;)[D
+
+[A[A[C[C[C[C[C[C[C[C
+[D[D[D[A[A[C[C[B[B[C[A[A[C[C[C[C[C)
