d90313c6bb5eb5f4ca8d655a3db8509fb574a5ea
R33dah <99521096+R33dah@users.noreply.github.com>
2022-03-22 00:20:24 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,4 +13,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
