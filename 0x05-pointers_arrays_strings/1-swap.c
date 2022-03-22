2416302d385e91803525d7d5b43cfef39e3b333c
R33dah <99521096+R33dah@users.noreply.github.com>
2022-03-22 01:33:14 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,21 +1,15 @@
-#include "MAIN_H"
-
+#include "main.h"
 /**
- *swap
-[A[C[C[C[C[C*
- * @: number 1.
- *
- * @b: munber 2.
- *
- *Return 0[D[D:[A[A[A[A[D[D[D[D[Da: number 1.[B[B[B
+ * swap_int - function to swap values of two integers
+ * @a: pointer
+ * @b: pointer
+ * Return: void
  */
+void swap_int(int *a, int *b)
+{
+	*a += *b;
 
-int main(void)
-{[A[C[C[C[C[C[C[C[C[C[C[C[Cvoid sw
-int c;
+	*b = *a - *b;
 
-c =a *a;[D[D[C[[C[C[C[C[Cc = *a;
-*a *b;
-[A[A[C[C[C[C[C=[C b*;
-*b = c*;
+	*a = *a - *b;
 }
