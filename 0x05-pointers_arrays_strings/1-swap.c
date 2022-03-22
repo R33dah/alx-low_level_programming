1a0af4fba3db369db99c153779e0b0a36d861dde
r33edah <little.musahh@gmail.com>
2022-03-22 01:08:14 +0100

    2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "MAIN_H"
+
+/**
+ *swap
+[A[C[C[C[C[C*
+ * @: number 1.
+ *
+ * @b: munber 2.
+ *
+ *Return 0[D[D:[A[A[A[A[D[D[D[D[Da: number 1.[B[B[B
+ */
+
+int main(void)
+{[A[C[C[C[C[C[C[C[C[C[C[C[Cvoid sw
+int c;
+
+c =a *a;[D[D[C[[C[C[C[C[Cc = *a;
+*a *b;
+[A[A[C[C[C[C[C=[C b*;
+*b = c*;
+}
