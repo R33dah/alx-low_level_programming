6b04472674913b0fa579977797146ba7698d12f0
r33edah <little.musahh@gmail.com>
2022-03-23 23:39:49 +0100

    new

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,42 +1,25 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * print_number - check the code for Holberton School students.
- * @n: is a int number
- * Return: Always 0.
- */
-
-
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
-int cont = 1, num = n;
+	unsigned int x;
 
-if (n < 0)
-_putchar('-');
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
 
-while (num / 10 != 0)
-{
-cont = cont * 10;
-num = num / 10;
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
-
-while ((n / cont != 0) && (cont != 1))
-{
-_putchar(abs((n / cont)) + '0');
-
-n = n % cont;
-
-if (cont > 1)
-cont = cont / 10;
-
-while ((abs(n) < cont) &&(cont > 1))
-{
-_putchar('0');
-cont = cont / 10;
-}
-}
-
-_putchar(abs(n) + '0');
-}
-
