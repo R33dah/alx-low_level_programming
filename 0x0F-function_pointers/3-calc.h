c1fdf880a8e23eb86da430710fc09a1916c9c2a3
R33dah <99521096+R33dah@users.noreply.github.com>
2022-04-13 22:42:27 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,17 +1,16 @@
-#ifndef _CALC_H_
-#define _CALC_H
+#ifndef CALC_H
+#define CALC_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct op - struct op
+ * struct op - Struct op
  *
  * @op: The operator
- * @f: the function associated
- *
+ * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
