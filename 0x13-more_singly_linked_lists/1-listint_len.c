884bb0726681eab1e937bfa3fa18f8de81fa9dfd
r33edah <little.musahh@gmail.com>
2022-04-25 21:00:55 +0100

    function that returns the number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * struct listint_s - singly linked list
+  * @n: integer
+  * @next: points to the next node
+  *
+  * Return: The number of elements in a linked list
+  */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	if (h != NULL)
+	{
+		while (h)
+		{
+			h = h->next;
+			count++;
+		}
+	}
+
+	return (count);
+}
