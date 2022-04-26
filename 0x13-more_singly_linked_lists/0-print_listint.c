bba6f9fc9d31fb62b3e9affaf17b360dca75ccde
r33edah <little.musahh@gmail.com>
2022-04-26 23:30:47 +0100

    function that counts element

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,19 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t nnodes = 0;
 
-	if (h != NULL)
-
+	while (h != NULL)
 	{
-		while (h)
-
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
 	}
-
-	return (count);
+	return (nnodes);
 }
