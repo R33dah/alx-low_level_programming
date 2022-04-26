4225432c3de462ccc26b4f84ed5ee96e64d2bd9d
r33edah <little.musahh@gmail.com>
2022-04-27 00:15:09 +0100

    function that counts element in linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * struct listint_s - singly linked list
-  * @n: integer
-  * @next: points to the next node
-  *
-  * Return: The number of elements in a linked list
-  */
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
 size_t listint_len(const listint_t *h)
 {
-        int count = 0;
+	size_t num = 0;
 
-        if (h != NULL)
-        {
-                while (h)
-                {
-                        h = h->next;
-                        count++;
-                }
-        }
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
 
-        return (count);
+	return (num);
 }
-
