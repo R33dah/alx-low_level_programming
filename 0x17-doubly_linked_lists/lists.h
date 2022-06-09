e2fd8df47ca17c74c35a99e73fc91772ca9067d1
R33dah <99521096+R33dah@users.noreply.github.com>
2022-06-09 23:50:14 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,5 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _LISTS_H
+#define _LISTS_H
 
 /**
  * struct dlistint_s - doubly linked list
@@ -26,10 +22,11 @@ size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *create_node(unsigned int n, void *next, void *prev);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_first_dnode(dlistint_t **head);
 
-#endif /* LISTS_H */
+#endif
